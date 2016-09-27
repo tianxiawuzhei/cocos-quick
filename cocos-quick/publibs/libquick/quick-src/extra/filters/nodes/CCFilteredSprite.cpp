bed666c0e48063eab11de780a2a87d1e331840b8
zhaoyuqi <1064639871@qq.com>
2016-09-27 18:52:18 +0800

    filter错误

--- cocos-quick/publibs/libquick/quick-src/extra/filters/nodes/CCFilteredSprite.cpp
+++ cocos-quick/publibs/libquick/quick-src/extra/filters/nodes/CCFilteredSprite.cpp
@@ -50,8 +50,12 @@ void FilteredSprite::draw(Renderer *renderer, const Mat4 &transform, uint32_t fl
     if(_insideBounds)
     {
         // normal effect: order == 0
-        _quadCommand.init(_globalZOrder, _texture->getName(), getGLProgramState(), _blendFunc, &_quad, 1, transform);
-        renderer->addCommand(&_quadCommand);
+//        _quadCommand.init(_globalZOrder, _texture->getName(), getGLProgramState(), _blendFunc, &_quad, 1, transform);
+//        renderer->addCommand(&_quadCommand);
+       _trianglesCommand.init(_globalZOrder, _texture->getName(), getGLProgramState(), _blendFunc, _polyInfo.triangles, transform, flags);
+       
+       
+       renderer->addCommand(&_trianglesCommand);
 
 //        for(auto &command : _pCommand) {
 //            QuadCommand &q = std::get<2>(command);
